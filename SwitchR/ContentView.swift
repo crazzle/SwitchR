2b710525112280884353d5d76ce2f34b57eba573
Mark K <mark.keinhoerster@gmail.com>
2021-03-06 20:41:37 +0100

    Initial Commit

---
+++ SwitchR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwitchR
+//
+//  Created by Mark Keinhörster on 06.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwitchR/SwitchRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchRApp.swift
+//  SwitchR
+//
+//  Created by Mark Keinhörster on 06.03.21.
+//
+
+import SwiftUI
+
+@main
+struct SwitchRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
