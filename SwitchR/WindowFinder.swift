96e1f463a47e7e7d1ed6413265e66386a97e7dd2
Mark K <mark.keinhoerster@gmail.com>
2022-01-18 22:22:07 +0100

    Clean-Up a bit and add comments

--- SwitchR/WindowFinder.swift
+++ SwitchR/WindowFinder.swift
@@ -12,8 +12,10 @@ import RxRelay
 
 typealias UIWindow = (id: CGWindowID, axapp: AXUIElement, axwindow: AXUIElement, name: String, title: String)
 typealias UIWindowNeighbours = (previous: UIWindow, next: UIWindow)
-typealias UIWindowSelection = (Int, ControlAction, UIWindow?)
-typealias UIWindowSelectable = (Int, ControlAction, Array<UIWindow>)
+typealias SelectedUIWindow = (Int, ControlAction, UIWindow?)
+typealias UIWindowList = (Int, ControlAction, Array<UIWindow>)
+
+let UNDEFINED_WINDOW_ID : Int = 0
 
 class WindowFinder {
     let disposableBag: DisposeBag = DisposeBag()
@@ -25,8 +27,8 @@ class WindowFinder {
     init(stream: Observable<ControlAction>) {
         self.stream = stream.map({checkAccessibilityTrusted(action: $0)})
         self.pickStream = self.stream.filter({$0 != .confirm})
-            .map({(0, $0, getRelevantWindows())})
-            .scan((0, ControlAction.next, Optional.none)){select(acc: $0, value: $1)}.map { $0.2 }
+            .map({(UNDEFINED_WINDOW_ID, $0, getRelevantWindows())})
+            .scan((UNDEFINED_WINDOW_ID, ControlAction.next, Optional.none)){select(acc: $0, value: $1)}.map { $0.2 }
         self.confirmationStream = self.stream.filter({$0 == .confirm})
         self.imageStream = self.pickStream.map{ (captureImage(window: $0), $0!.name, $0!.title) }
         self.confirmationStream.withLatestFrom(self.pickStream)
@@ -36,42 +38,83 @@ class WindowFinder {
     }
 }
 
+/**
+ Retrieve the relevant windows based on the currently running applications
+ */
 func getRelevantWindows() -> [UIWindow] {
     let apps = NSWorkspace.shared.runningApplications.filter{$0.activationPolicy == .regular}
-    let windows: Array<UIWindow> = apps.map{
-        let pid = $0.processIdentifier
-        let name = $0.localizedName
-        let app = AXUIElementCreateApplication(pid)
-        guard let windowsList = getAXAttribute(container: app, attribute: kAXWindowsAttribute, to: Array<AXUIElement>()) else {
-            return Array<UIWindow>()
-        }
-        return windowsList.map{
-            let id = IDFinder.getWindowID($0)
-            guard let title = getAXAttribute(container: $0, attribute: kAXTitleAttribute, to: String()) else {
-                return (id, app, $0, name!, "")
-            }
-            return (id, app, $0, name!, title)
-        }
-    }.reduce([], +).filter {
-        guard let minimized = getAXAttribute(container: $0.axwindow, attribute: kAXMinimizedAttribute, to: Bool()) else {
-            return false
-        }
-        return !minimized
-    }.filter {
-        guard let enabled = getAXAttribute(container: $0.axwindow, attribute: kAXEnabledAttribute, to: Bool()) else {
-            return true
-        }
-        return enabled
-    }.sorted(by: {$0.id > $1.id})
+    let windows: Array<UIWindow> = apps.map(getActiveWindowsForApp)
+        .reduce([], +)
+        .filter(windowIsNotMinimized)
+        .filter(windowIsEnabled)
+        .sorted(by: {$0.id > $1.id})
     return windows
 }
 
-func select(acc: UIWindowSelection, value: UIWindowSelectable) -> UIWindowSelection{
+/**
+ Retrieve all active windows of a running application
+ */
+func getActiveWindowsForApp(app: NSRunningApplication) -> [UIWindow]{
+    let pid = app.processIdentifier
+    let name = app.localizedName
+    let app = AXUIElementCreateApplication(pid)
+   
+    /**
+     Retrieve all  windows for an NSRunningApplication
+     */
+    guard let windowsList = getAXAttribute(container: app, attribute: kAXWindowsAttribute, to: Array<AXUIElement>()) else {
+        return Array<UIWindow>()
+    }
+    
+    /**
+     Map  AXUIElemenst to a UIWindows
+     */
+    return windowsList.map{
+        let id = IDFinder.getWindowID($0)
+        guard let title = getAXAttribute(container: $0, attribute: kAXTitleAttribute, to: String()) else {
+            return (id, app, $0, name!, "")
+        }
+        return (id, app, $0, name!, title)
+    }
+}
+
+/**
+ Check if a AXUIWindow is not minimized
+ */
+func windowIsNotMinimized(window: UIWindow) -> Bool{
+    guard let minimized = getAXAttribute(container: window.axwindow,
+                                         attribute: kAXMinimizedAttribute,
+                                         to: Bool()) else {
+        return false
+    }
+    return !minimized
+}
+
+/**
+ Check if a AXUIWindow is enabled and a user can interact with it.
+ E.g. this is needed for the Finder Application which runs almost all the
+ time containing a disabled window
+ */
+func windowIsEnabled(window: UIWindow) -> Bool{
+    guard let enabled = getAXAttribute(container: window.axwindow,
+                                       attribute: kAXEnabledAttribute,
+                                       to: Bool()) else {
+        return true
+    }
+    return enabled
+}
+
+/**
+ Selects a new window by looking at the index of the current selection and action
+ */
+func select(acc: SelectedUIWindow, value: UIWindowList) -> SelectedUIWindow{
     let (lastIndex, _, _) = acc
     let (_, newAction, currentWindows) = value
     let nextIndex : Int = {
         switch newAction{
+        // Return the index of the next window or the first one
         case .next: return lastIndex < currentWindows.count - 1 ? lastIndex + 1 : 0
+        // Return the index of the previous window or the last oneπ
         case .previous: return lastIndex > 0 ? lastIndex - 1 : currentWindows.count - 1
         default: return lastIndex
         }
@@ -79,6 +122,12 @@ func select(acc: UIWindowSelection, value: UIWindowSelectable) -> UIWindowSelect
     return currentWindows.isEmpty ? (nextIndex, newAction, .none) : (nextIndex, newAction, currentWindows[nextIndex])
 }
 
+/**
+ Focusing not only an app, but a specified window of an app. All three attributes have to be set:
+ - kAXFrontmostAttribute to set the app as active and selected
+ - kAXMainAttribute make the window the main window of the app
+ - kAXFocusedAttribute to set the keyboard focus for the main window
+ */
 func setFocus(window: UIWindow?){
     if let win = window {
         AXUIElementSetAttributeValue(win.axapp, kAXFrontmostAttribute as CFString, kCFBooleanTrue)
@@ -87,6 +136,9 @@ func setFocus(window: UIWindow?){
     }
 }
 
+/**
+ Capturing a screenshot from a window using it's CGWindowID
+ */
 func captureImage(window: UIWindow?) -> NSImage?{
     guard let window = window else {
         return .none
@@ -99,11 +151,19 @@ func captureImage(window: UIWindow?) -> NSImage?{
     return NSImage(cgImage:windowImage, size:NSZeroSize)
 }
 
-func getAXAttribute<T>(container: AXUIElement, attribute: String, to type: T) -> T?{
-    var ax: CFTypeRef?
-    AXUIElementCopyAttributeValue(container, attribute as CFString, &ax)
-    guard let axResult = ax else { return .none }
-    return axResult as? T
+/**
+ Checks if the needed accessibilty access  is granted.
+ Needed access rights:
+ - check if SwitchR is a trusted accessibilty client to get information about running apps
+ - check if SwitchR is allowed to take screen captures for app thumbnails
+ */
+func checkAccessibilityTrusted(action: ControlAction) -> ControlAction{
+    switch (gotAXIAccess() && gotCaptureAccess()) {
+    case true:
+        return action
+    default:
+        return .abort
+    }
 }
 
 func gotAXIAccess() -> Bool{
@@ -114,11 +174,12 @@ func gotCaptureAccess() -> Bool{
     return CGPreflightScreenCaptureAccess()
 }
 
-func checkAccessibilityTrusted(action: ControlAction) -> ControlAction{
-    switch (gotAXIAccess() && gotCaptureAccess()) {
-    case true:
-        return action
-    default:
-        return .abort
-    }
+/**
+ Helper function to retrieve an attribute from an AXUIElement which is either an app or a window.
+ */
+func getAXAttribute<T>(container: AXUIElement, attribute: String, to type: T) -> T?{
+    var ax: CFTypeRef?
+    AXUIElementCopyAttributeValue(container, attribute as CFString, &ax)
+    guard let axResult = ax else { return .none }
+    return axResult as? T
 }
